41b2d5e37900eecd5a2e2179770d926522a9e8d6
Reuben Bishop <reubenjamesbishop@gmail.com>
2024-05-18 15:51:03 +0930

    Add simple route for voice-to-text for proof of concept.

--- src/main.py
+++ src/main.py
@@ -1,12 +1,26 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
+from dotenv import load_dotenv
+from pydantic import BaseModel
+import os
+
+import assemblyai as aai
+
+load_dotenv()
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/voice-to-text")
+def create_transcription(file: UploadFile): 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    print(file.content_type)
+    print(file.filename)
+
+    aai.settings.api_key = os.getenv('ASSEMBLY_AI_API_KEY')
+
+    transcriber = aai.Transcriber()
+    transcript = transcriber.transcribe(file.file)
+
+    return {"transcription": transcript.text}
+
+
