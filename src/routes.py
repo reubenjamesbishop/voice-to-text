9e715bc98512241e0c77f2d666d739236025ff70
Reuben Bishop <reubenjamesbishop@gmail.com>
2024-05-18 23:15:25 +0930

    Update text to speech route to include error handling

--- src/routes.py
+++ src/routes.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import logging
 
 from .models import Transcript, TranscriptResponse
-from .AudioTranscriber import AudioTranscriber
+from .AudioTranscriber import transcribe_file
 
 router = APIRouter()
 
@@ -13,22 +13,28 @@ async def create_transcription(file: UploadFile):
     if file.content_type != "audio/mpeg":
         raise HTTPException(status_code=400, detail="Invalid file type. Please upload an MP3 file!")
 
+    # Get the text from the mp3 file
     try:
-        transcript_text = AudioTranscriber().transcribe_file(file.file) 
-        # transcript_text = "some new test text" 
+        transcript_text = "some new test text" 
+        # transcript_text = transcribe_file(file.file)
     except Exception as e:
-        print(f'Failed to transcribe audio. Message: {e}')
+        raise HTTPException(status_code=500, detail=f'Failed to transcribe audio. Message: {e}')
     
-    current_time: datetime = datetime.now()
-    t = Transcript(
-        audio_source=file.filename,
-        content=transcript_text,
-        status="complete",
-        created_time=f"{current_time}",
-        updated_time=f"{current_time}",
-    )
-    await t.create()
+    # Save the transcription information in the database
+    try: 
+        current_time: datetime = datetime.now()
+        t = Transcript(
+            audio_source=file.filename,
+            content=transcript_text,
+            status="complete",
+            created_time=f"{current_time}",
+            updated_time=f"{current_time}",
+        )
+        await t.create()
+    except:
+        raise HTTPException(status_code=500, detail=f'Failed to save transcription. Message: {e}')
 
+    # Return the response to the client, with a formatted transcription ID
     response = TranscriptResponse(
         id=str(t.id),
         audio_source=t.audio_source,
@@ -37,6 +43,5 @@ async def create_transcription(file: UploadFile):
         created_time=t.created_time,
         updated_time=t.updated_time,
     )
-
     return response
 
